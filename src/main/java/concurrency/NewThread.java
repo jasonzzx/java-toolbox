bdefd0c077e33a2b0b6d3c516c3df7a34f924f05
Jason Zhong <jasonzzx@gmail.com>
2021-03-05 17:18:37 -0500

    Add CompletableFuture example

--- src/main/java/concurrency/NewThread.java
+++ src/main/java/concurrency/NewThread.java
@@ -1,10 +1,14 @@
 package concurrency;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NewThread {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Thread threadExtend = new ThreadExtend();
         Thread threadImplRunnable = new Thread(new ThreadImplRunnable());
         Thread threadArrowRunnable = new Thread(() -> System.out.println("Thread implements Runnable with Arrow Function"));
@@ -18,6 +22,31 @@ public class NewThread {
         threadImplCallable.start();
 
         System.out.println(futureTask.get());
+
+        CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Future 1 Done";
+        }).completeOnTimeout("Empty Value on timeout", 1000, TimeUnit.MILLISECONDS);
+
+        CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Future 2 Done";
+        }).completeOnTimeout("Empty Value on timeout", 1000, TimeUnit.MILLISECONDS);;
+
+        System.out.println(
+                Stream.of(completableFuture1, completableFuture2)
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.joining(" ; "))
+        );
+
     }
 }
 
