e949a6343e994e58e75fb5b2df97b8c79e96730a
Jason Zhong <jasonzzx@gmail.com>
2022-02-14 16:12:51 -0500

    Create Convert.java

---
+++ src/main/java/Convert.java
@@ -0,0 +1,9 @@
+public class Convert {
+    public static void main(String[] args) {
+        // 1D: String, char, int, boolean,
+        // 1D: []
+        // List (ArrayList, LinkedList), Queue, Stack
+        // Set (HashSet, TreeSet)
+        // Map (HashMap)
+    }
+}
