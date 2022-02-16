e069e2f7569978b8eeb2dbd11426e3be9840d897
Jason Zhong <jasonzzx@gmail.com>
2022-02-16 16:49:29 -0500

    Update Convert.java

--- src/main/java/Convert.java
+++ src/main/java/Convert.java
@@ -22,10 +22,45 @@ public class Convert {
 
         // int array sort
         int[] arr = { 4, 3, 1, 2, 5 };
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr)); // asc
-        // desc
+        Arrays.sort(arr); //asc
+        System.out.println(Arrays.toString(arr));
         int[] descArr = IntStream.of(arr).boxed().sorted(Comparator.reverseOrder()).mapToInt(i -> i).toArray();
         System.out.println(Arrays.toString(descArr));
+
+        // String to num[] with max min avg
+        System.out.println("12345".chars().max().getAsInt() - '0');
+        System.out.println("12345".chars().min().getAsInt() - '0');
+        System.out.println((int)"12345".chars().average().getAsDouble() - '0');
+
+        // List <-> array[] (primitive)
+        int[] array = { 4, 3, 1, 2, 5 };
+        List<Integer> intList = Arrays.stream(array).boxed().collect(Collectors.toList());
+        System.out.println(intList);
+        int[] bArray = intList.stream().mapToInt(i -> i).toArray();
+        System.out.println(Arrays.toString(bArray));
+
+        // List <-> array[] (Object/String)
+        String[] arrayStr = {"a", "b", "c"};
+        List<String> strList = Arrays.asList(arrayStr); // can't do this with primitive array as above int[]
+        System.out.println(strList);
+        String[] bArrayStr = strList.toArray(String[]::new);
+        System.out.println(Arrays.toString(bArrayStr));
+
+        // List -> ArrayList/LinkedList
+        ArrayList<Integer> arrayList = new ArrayList<>(intList);
+        System.out.println(arrayList);
+        LinkedList<String> linkedList = new LinkedList<>(strList);
+        System.out.println(linkedList);
+
+        // List operation
+        arrayList.add(10);
+        linkedList.add("e");
+        linkedList.addLast("f");
+        linkedList.addFirst("z");
+        System.out.println(linkedList);
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        linkedList.getFirst();
+        linkedList.getLast();
     }
 }
