393006c1b4d99770599dc0b945c40a912228894c
Jason Zhong <jasonzzx@gmail.com>
2022-03-02 21:12:53 -0500

    Collection

--- src/main/java/CollectionTest.java
+++ src/main/java/CollectionTest.java
@@ -1,33 +1,41 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CollectionTest {
     public static void main(String[] args) {
         PriorityQueue<Integer> priorityQueueAse = new PriorityQueue<>();
-        priorityQueueAse.add(3);
-        priorityQueueAse.add(1);
-        priorityQueueAse.add(2);
-        priorityQueueAse.add(4);
+        priorityQueueAse.add(3);priorityQueueAse.add(1);priorityQueueAse.add(2);priorityQueueAse.add(4);
         System.out.println(priorityQueueAse);
         System.out.println(priorityQueueAse.peek());
         System.out.println(priorityQueueAse.remove(2));
-        System.out.println(priorityQueueAse);
 
         PriorityQueue<Integer> priorityQueueDesc = new PriorityQueue<>(Collections.reverseOrder());
-        priorityQueueDesc.add(3);
-        priorityQueueDesc.add(1);
-        priorityQueueDesc.add(2);
-        priorityQueueDesc.add(4);
+        priorityQueueDesc.add(3);priorityQueueDesc.add(1);priorityQueueDesc.add(2);priorityQueueDesc.add(4);
         System.out.println(priorityQueueDesc);
         System.out.println(priorityQueueDesc.peek());
         System.out.println(priorityQueueDesc.remove(2));
-        System.out.println(priorityQueueDesc);
 
         TreeSet<Integer> treeSet = new TreeSet<>();
-        treeSet.add(3);
-        treeSet.add(1);
-        treeSet.add(2);
+        treeSet.add(3);treeSet.add(1);treeSet.add(2);
+        treeSet.remove(2);
         System.out.println(treeSet);
+
+        Deque<Integer> deque = new LinkedList<>(); // FIFO / FILO
+        deque.add(1);deque.add(2);deque.add(3);deque.add(4);deque.add(5);
+        System.out.println(deque.pollFirst()); // 1
+        System.out.println(deque.pollLast()); // 5
+        System.out.println(deque.peekFirst()); // 2
+        System.out.println(deque.peekLast()); // 4
+        System.out.println(deque);
+
+        Queue<Integer> queue = new LinkedList<>(); // FIFO
+        queue.add(1);queue.add(2);queue.add(3);queue.add(4);queue.add(5);
+        System.out.println(queue.poll()); // 1
+        System.out.println(queue.poll()); // 2
+
+        Stack<Integer> stack = new Stack<>(); // FILO
+        stack.push(1);stack.push(2);stack.push(3);stack.push(4);stack.push(5);
+        System.out.println(stack.pop()); // 5
+        System.out.println(stack.pop()); // 4
+
     }
 }
